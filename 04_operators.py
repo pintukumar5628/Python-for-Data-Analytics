5936a5bd691be3f4b9c3e0cb21f948bca56b3342
PINTU KUMAR SAH <126938670+pintukumar5628@users.noreply.github.com>
2024-08-04 23:57:13 +0530

    Add files via upload

---
+++ 04_operators.py
@@ -0,0 +1,30 @@
+# Arthametic Operater
+
+a = 7
+b = 4
+c = a + b  # Addition
+print(c)   # Output: 11
+
+a = 4 - 2   # Subtraction and assignment to 'a'
+print(a)    # Output: 2
+
+b = 6
+# b + 3  # This line is commented out; it doesn't affect the program
+b -= 3     # Equivalent to b = b - 3, which decrements b by 3
+print(b)   # Output: 3
+
+x = 5
+y = 3
+
+print(x > y)   # Output: True (5 is greater than 3)
+print(x < y)   # Output: False (5 is not less than 3)
+print(x == y)  # Output: False (5 is not equal to 3)
+print(x != y)  # Output: True (5 is not equal to 3)
+
+# Logical operators
+
+e = True or False
+print("True or False is ", True or False)
+print("True or True is ",True or True)
+print("False or True is ", False or True)
+print("False or False is ", False or False)
